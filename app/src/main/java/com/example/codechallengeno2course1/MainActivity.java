9bc5b2a56277a4f197820468c1546616cc552319
octavian <octavian.vilceanu@ligaac.ro>
2019-03-20 16:27:33 +0200

    change message Hello word to Happy birthday to...

---
+++ app/src/main/java/com/example/codechallengeno2course1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.codechallengeno2course1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e("MainActivity","Inside on create Method");
+    }
+}
